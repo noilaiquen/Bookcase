b5fb2056c0abf9223b3dd4c43133de89f4a31cf1
nguyenthanhbinh255@gmail.com <nguyenthanhbinh255@gmail.com>
2018-05-07 18:19:47 +0700

    prepare project

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import Bookcase from './app/App';
 
-AppRegistry.registerComponent('BookcaseApp', () => Bookcase);
+AppRegistry.registerComponent('Bookcase', () => Bookcase);
