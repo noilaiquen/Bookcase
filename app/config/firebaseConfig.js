028e39e7fc19fd49a9bcb896217021e121a91a3d
Thanh Binh Nguyen <nguyenthanhbinh255@gmail.com>
2018-05-18 11:04:24 +0700

    Update firebaseConfig.js

--- app/config/firebaseConfig.js
+++ app/config/firebaseConfig.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-   apiKey: '',
-   authDomain: '',
-   databaseURL: '',
-   projectId: '',
-   storageBucket: '',
-   messagingSenderId: ''
+   apiKey: 'API_KEY',
+   authDomain: 'AUTH_DOMAIN',
+   databaseURL: 'DATABASE_URL',
+   projectId: 'PROJECT_ID',
+   storageBucket: 'STORAGE_BUCKET',
+   messagingSenderId: 'MASSAGING_SENDER_ID'
 };
  
 export const firebaseApp = firebase.initializeApp(config);
