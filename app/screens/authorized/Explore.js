4d769046acfcbc87b14f417df84599a41854254b
nguyenthanhbinh255@gmail.com <nguyenthanhbinh255@gmail.com>
2018-06-04 16:55:57 +0700

    add explore

--- app/screens/authorized/Explore.js
+++ app/screens/authorized/Explore.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import {
-   StyleSheet,
    Text,
    View,
+   ScrollView
 } from 'react-native';
 import { Header } from '../../components';
+import { appFont } from '../../config/constants';
+
+const list = [
+   {
+      title: '#EF5350',
+      color: '#EF5350'
+   },
+   {
+      title: '#F44336',
+      color: '#F44336'
+   },
+   {
+      title: '#E91E63',
+      color: '#E91E63'
+   },
+   {
+      title: '#8E24AA',
+      color: '#8E24AA'
+   },
+   {
+      title: '#D32F2F',
+      color: '#D32F2F'
+   },
+   {
+      title: '#00695C',
+      color: '#00695C'
+   },
+];
 
 export default class Explore extends Component {
    static navigationOptions = () => ({
@@ -19,23 +47,33 @@ export default class Explore extends Component {
 
    render() {
       return (
-         <View style={styles.container}>
-            <Text style={styles.title}>Explore</Text>
-         </View>
+         <ScrollView contentContainerStyle={styles.container}>
+            {list.map(item => (
+               <View style={[styles.listItem, { backgroundColor: item.color }]} key={item.title}>
+                  <Text style={styles.title}>{item.title}</Text>
+               </View>
+            ))}   
+         </ScrollView>
       );
    }
 }
 
-const styles = StyleSheet.create({
+const styles = {
    container: {
-      flex: 1,
+      padding: 10,
+      backgroundColor: '#FFF'
+   },
+   listItem: {
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+      height: 100,
+      borderRadius: 5,
+      elevation: 1.5,
+      marginBottom: 10
    },
    title: {
       fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
+      fontFamily: appFont,
+      color: '#FFF'
    },
-});
+};
